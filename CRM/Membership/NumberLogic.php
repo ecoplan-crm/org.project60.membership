cb8d01100cf23ee6ed83db97ec6ebf7ab87f1d33
Torben Bertram <100374351+eptbertram@users.noreply.github.com>
2022-12-29 17:30:29 +0100

    Update NumberLogic.php

--- CRM/Membership/NumberLogic.php
+++ CRM/Membership/NumberLogic.php
@@ -232,7 +232,8 @@ class CRM_Membership_NumberLogic {
                     $cid += substr($matches['offset'], 1);
                 }
             }
-            $number = preg_replace('#\{cid(?P<offset>[+-][0-9]+)?\}#', $cid, $number);
+            // $number = preg_replace('#\{cid(?P<offset>[+-][0-9]+)?\}#', $cid, $number);
+            $number = preg_replace('#\{mid(?P<offset>[+-][0-9]+)?\}#', sprintf('%04d', $mid), $number);            
         }
 
         // TODO: implement {seq:x} pattern
